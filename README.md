c027827dda7e07a7fb8ad59bc25e9da52f480311
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2021-11-12 13:05:01 +0100

    Second commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Inrae Sixtine Vigne
-
---
+++ src/main/java/fr/inrae/sixtine/SixtineModule.java
@@ -0,0 +1,42 @@
+//******************************************************************************
+// OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
+// Copyright © INRA 2019
+// Contact: vincent.migot@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+//******************************************************************************
+package fr.inrae.sixtine;
+
+import java.util.List;
+import org.apache.jena.riot.Lang;
+import org.opensilex.OpenSilexModule;
+import org.opensilex.sparql.SPARQLConfig;
+import org.opensilex.sparql.SPARQLModule;
+import org.opensilex.sparql.extensions.OntologyFileDefinition;
+import org.opensilex.sparql.extensions.SPARQLExtension;
+
+/**
+ * Sixtine module implementation
+ */
+public class SixtineModule extends OpenSilexModule implements SPARQLExtension {
+
+    @Override
+    public List<OntologyFileDefinition> getOntologiesFiles() throws Exception {
+        List<OntologyFileDefinition> list = SPARQLExtension.super.getOntologiesFiles();
+        list.add(new OntologyFileDefinition(
+                "https://sixtine.inrae.fr/ontology#",
+                "ontologies/inrae-sixtine.owl",
+                Lang.RDFXML,
+                "inrae-sixtine"
+        ));
+
+        SPARQLConfig sparqlConfig = getOpenSilex().getModuleConfig(SPARQLModule.class, SPARQLConfig.class);
+        list.add(new OntologyFileDefinition(
+                sparqlConfig.baseURI() + "set/germplasms",
+                "ontologies/species.ttl",
+                Lang.TTL,
+                sparqlConfig.baseURIAlias() + "-species"
+        ));
+
+        return list;
+    }
+
+}
